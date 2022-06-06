f94792b870970fd53abb1887f01fda5258da26e4
Jason Wicker <jason.wicker@nent.dev>
2022-06-06 15:42:09 -0600

    markdown: add support for offsite links to always open new tab (#356)
    * feat(✨): external link detection
    
    automatically configure links with offiste URLs to open in new tab
    
    * ci(⚙️): cleaned up files before publish

--- packages/core/src/components/n-content-markdown/markdown/__mocks__/remarkable.worker.ts
+++ packages/core/src/components/n-content-markdown/markdown/__mocks__/remarkable.worker.ts
@@ -1,15 +1,27 @@
 /* istanbul ignore file */
-// @ts-nocheck
 
 import { Remarkable } from 'remarkable'
-
+import { linkify } from 'remarkable/linkify'
 export async function renderMarkdown(
   content: string,
 ): Promise<string | undefined> {
   const markdown = new Remarkable({
     html: true,
     typographer: true,
+    breaks: true,
   })
+  markdown.use(linkify, {})
+  var defaultLinkOpen = markdown.renderer.rules.link_open
+  markdown.renderer.rules.link_open = (
+    tokens: any,
+    idx: number,
+    options: any,
+  ) => {
+    if (tokens[idx].href.startsWith('http'))
+      options.linkTarget = '_blank'
+    else options.linkTarget = ''
+    return defaultLinkOpen(tokens, idx, options)
+  }
 
   return markdown.render(content)
 }
